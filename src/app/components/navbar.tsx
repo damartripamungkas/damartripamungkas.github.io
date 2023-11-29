67ee72891c53aaf9b3687155ed5ea6646a1a7336
damartripamungkas <damartripamungkas@github.com>
2023-11-29 21:04:12 +0700

    feat: add sidebar menu for mobile view

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,8 @@
 "use client"
-import { usePathname } from "next/navigation"
 import { useTheme } from "next-themes"
+import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
+import { FiMenu, FiMoon, FiSun } from "react-icons/fi"
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -14,29 +16,42 @@ const classNames = (...classes: string[]) => {
 }
 
 export default function Navbar() {
-  const pathName = usePathname()
   const { theme, setTheme } = useTheme()
+  const [isClient, setIsClient] = useState(false)
+  const pathName = usePathname()
+  const antonymTheme = theme == "dark" ? "light" : "dark"
+  const iconTheme = antonymTheme == "dark" ? <FiMoon size={22} /> : <FiSun size={22} />
+  const onClickSetTheme = () => {
+    setTheme(antonymTheme)
+    document.body.setAttribute("data-theme", antonymTheme)
+  }
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   return (
     <nav className="flex h-16 justify-between">
       <div className="flex">
         <div className="flex flex-shrink-0 items-center">
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            className="text-gray-100"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="100%" height="100%" rx="16" fill="currentColor" />
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-              fill="black"
-            />
-          </svg>
+          <a href="/">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              className="text-neutral-content"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="100%" height="100%" rx="16" fill="currentColor" />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+                fill="black"
+              />
+            </svg>
+          </a>
         </div>
         <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
           {navigation.map((item) => (
@@ -56,40 +71,43 @@ export default function Navbar() {
         </div>
       </div>
 
-      <div className="flex">
-        <div className="flex flex-shrink-0 items-center">
-          <button
-            className="text-sm btn btn-circle btn-sm"
-            onClick={() => {
-              setTheme(theme == "dark" ? "light" : "dark")
-            }}
-          >
-            CT
-          </button>
+      <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+        <div className="flex">
+          <div className="flex flex-shrink-0 items-center">
+            <button className="btn btn-circle btn-sm" onClick={onClickSetTheme}>
+              {isClient ? iconTheme : <span className="loading loading-spinner"></span>}
+            </button>
+          </div>
         </div>
       </div>
 
-      {/* <Disclosure.Panel className="sm:hidden">
-        <div className="space-y-1 pt-2 pb-3">
-          {navigation.map((item) => (
-            <Disclosure.Button
-              key={item.name}
-              as="a"
-              href={item.href}
-              className={classNames(
-                pathname === item.href
-                  ? "bg-slate-50 border-slate-500 text-slate-700"
-                  : "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800",
-                "block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-              )}
-              aria-current={pathname === item.href ? "page" : undefined}
-            >
-              {item.name}
-            </Disclosure.Button>
-          ))}
+      <div className="-mr-2 flex items-center sm:hidden">
+        <div className="drawer z-50">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            <label htmlFor="my-drawer" role="button" className="btn btn-sm">
+              <FiMenu size={19} />
+            </label>
+          </div>
+          <div className="drawer-side overflow-hidden">
+            <label htmlFor="my-drawer" className="drawer-overlay"></label>
+            <ul className="menu p-5 w-8/12 min-h-full bg-base-200 text-base-content">
+              {[
+                ...navigation.map((it, index: number) => (
+                  <li key={index}>
+                    <a href={it.href}>{it.name}</a>
+                  </li>
+                )),
+                <li key={navigation.length} className="pt-3">
+                  <a onClick={onClickSetTheme}>
+                    {isClient ? iconTheme : <span className="loading loading-spinner"></span>}
+                  </a>
+                </li>
+              ]}
+            </ul>
+          </div>
         </div>
-        <div className="border-t border-gray-200 pt-4 pb-3"></div>
-      </Disclosure.Panel> */}
+      </div>
     </nav>
   )
 }
