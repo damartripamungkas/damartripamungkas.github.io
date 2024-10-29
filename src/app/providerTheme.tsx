188c3166629eee26911673f48f75f174afac3476
damartripamungkas <damartripamungkas@gmail.com>
2024-10-29 19:12:16 +0700

    chore: remove change theme

--- src/app/providerTheme.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import React from "react"
-import { ThemeProvider } from "next-themes"
-
-export default function Provider({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>
-}
